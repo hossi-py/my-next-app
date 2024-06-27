84a7005a8b56d7cd04f11a52126e1919575d6e16
d220191 <yoonho.hwang@kbfg.com>
2024-06-27 13:34:05 +0900

    드래그 기능 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,27 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="relative w-[90%] md:w-[70%] h-[600px] bg-gray-200 rounded-lg flex items-center justify-center overflow-hidden">
-        <FallingAnimation>
-          <Chip color="default">Default</Chip>
-          <Chip color="primary">Primary</Chip>
-          <Chip color="secondary">Secondary</Chip>
-          <Chip color="success">Success</Chip>
-          <Chip color="warning">Warning</Chip>
-          <Chip color="danger">Danger</Chip>
-        </FallingAnimation>
+        <div className="relative w-[50%] h-full flex items-center justify-center">
+          <FallingAnimation>
+            <Chip color="default">Default</Chip>
+            <Chip
+              variant="shadow"
+              classNames={{
+                base: 'bg-gradient-to-br from-indigo-500 to-pink-500 border-small border-white/50 shadow-pink-500/30',
+                content: 'drop-shadow shadow-black text-white',
+              }}
+            >
+              Primary
+            </Chip>
+            <Chip color="secondary">Secondary</Chip>
+            <Chip color="success">Success</Chip>
+            <Chip color="warning">Warning</Chip>
+            <Chip color="danger">Danger</Chip>
+          </FallingAnimation>
+        </div>
 
-        {/* Card Skeleton */}
-        <div className="flex-grow flex items-center justify-center">
+        {/* Card */}
+        <div className="w-[50%] h-full flex items-center justify-center">
           <div className="w-full max-w-md h-full max-h-96 space-y-5 p-4 bg-white rounded-lg shadow-lg">
             <div className="rounded-lg bg-default-300 h-24"></div>
             <div className="space-y-3">
--- src/shared/ui/falling-animation.tsx
+++ src/shared/ui/falling-animation.tsx
@@ -1,11 +1,11 @@
 'use client';
 
 import React, { ReactNode, useEffect, useState } from 'react';
-import Draggable from 'react-draggable';
+import Draggable, { DraggableEventHandler } from 'react-draggable';
 
 // 떨어지는 위치와 회전 조절
 const getRandomPosition = () => ({
-  x: Math.random() * 200 - 100,
+  x: Math.random() * 200 - 300,
   y: Math.random() * 200 + 300,
   rotate: Math.random() * 60 - 30,
 });
@@ -20,29 +20,43 @@ const FallingChip = ({ child, index }: { child: ReactNode; index: number }) => {
     y: 0,
     rotate: 0,
   });
+  const [isDragging, setIsDragging] = useState<boolean>(false);
 
   useEffect(() => {
     setPosition(getRandomPosition());
   }, []);
+
+  const handleStart: DraggableEventHandler = () => {
+    setIsDragging(true);
+  };
+
+  const handleStop: DraggableEventHandler = () => {
+    setIsDragging(false);
+  };
 
   return (
     <div
       className="absolute"
       style={{
         top: `${-100}px`,
-        left: '50%',
+        left: '70%',
         transform: `translateX(-50%)`,
         animation: `fall-${index} 2s forwards`,
         animationDelay: `${index * 0.5}s`,
+        cursor: 'pointer',
       }}
     >
-      <Draggable>{child}</Draggable>
+      <Draggable onStart={handleStart} onStop={handleStop}>
+        {child}
+      </Draggable>
+      {/* 기울기가 설정되어 있으면 드래그 시 이동에 불편함이 있음 */}
       <style jsx>{`
         @keyframes fall-${index} {
           to {
             top: ${position.y}px;
-            left: calc(50% + ${position.x}px);
-            transform: translateX(-50%) rotate(${position.rotate}deg);
+            left: calc(70% + ${position.x}px);
+            transform: translateX(-50%)
+              ${isDragging ? '' : `rotate(${position.rotate}deg)`};
           }
         }
       `}</style>
@@ -52,7 +66,7 @@ const FallingChip = ({ child, index }: { child: ReactNode; index: number }) => {
 
 const FallingAnimation = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="overflow-hidden">
+    <div>
       {React.Children.map(children, (child, index) => (
         <FallingChip child={child} index={index} />
       ))}
